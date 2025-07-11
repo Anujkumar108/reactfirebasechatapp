e8a2dd0250bebf9e57b690e13d47fcf9489e841c
Anujkumar108 <141645976+Anujkumar108@users.noreply.github.com>
2025-07-11 14:09:33 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7WeZD_SSz7ctYhQp6yPeA90MXF9BEr-c",
+  authDomain: "chat-app-5e6a1.firebaseapp.com",
+  projectId: "chat-app-5e6a1",
+  storageBucket: "chat-app-5e6a1.firebasestorage.app",
+  messagingSenderId: "630022859000",
+  appId: "1:630022859000:web:a69e8b52562d73ee88b30d",
+  measurementId: "G-J30FJ4PCM4"
+};
+
+export const app = initializeApp(firebaseConfig);
